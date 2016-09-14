c14255ee2d5de2d9ca70609660474435899b549a
Natalochka1 <natatipaeva@mail.ru>
2016-09-14 13:33:10 +0300

    task5

---
+++ js/main-dist.js
@@ -0,0 +1,2 @@
+"use strict";$(document).ready(function(){$(".single-item").slick({dots:!0,arrows:!1})});
+//# sourceMappingURL=main-dist.js.map
--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-  
-})
+
+
+$(document).ready(function(){
+  $('.single-item').slick({
+        dots: true,
+        arrows: false,
+  });
+});
